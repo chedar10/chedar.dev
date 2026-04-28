b0c7f4d80ab4e723e1a83efe2c7054074035d015
Mason McKeever <cheeseyfries2023@gmail.com>
2026-04-28 07:24:23 -0700

    random commit

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.notFound(async (c) => {
+
+// Serving stuff
+app.get('*', async (c) => {
+    const response = await c.env.ASSETS.fetch(c.req.raw)
+
+    if (response.status < 400) {
+        return response
+    }
+
     const url = new URL(c.req.url)
     url.pathname = '/404.html'
-    const response = await c.env.ASSETS.fetch(url.toString())
-    return new Response(response.body, {
-        ...response,
-        status: 404
-    })
+    const errorPage = await c.env.ASSETS.fetch(url.toString())
+
+    return new Response(errorPage.body, {...errorPage,status: 404,epicness: 'none :('})
 })
 
 export default app
