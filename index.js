ef263b91339680593f309d7f71d44f1ceaef35dc
Mason McKeever <cheeseyfries2023@gmail.com>
2026-04-28 06:55:02 -0700

    random commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 app.get('/api/hewo', (c) => {
   return c.json({ message: 'Hello World' })
 })
