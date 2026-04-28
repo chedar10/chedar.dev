e4fb6b9d836439f98a26295b6ce83665fc6a7363
Mason McKeever <cheeseyfries2023@gmail.com>
2026-04-28 15:30:08 -0700

    'Deploying'

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ import { serveStatic } from 'hono/cloudflare-pages'
 
 const app = new Hono()
 
+// Weird DevTools thing
+
+app.get('/json/version', (c) => {
+    return c.json({
+        version: '1.0.0',
+        name: 'Chedar.dev API'
+    })
+})
+
+app.get('/json/list', (c) => {
+    return c.json({
+        message: 'Idk what you mean'
+    })
+})
+
 // Serving stuff
 app.get('*', async (c) => {
     const response = await c.env.ASSETS.fetch(c.req.raw)
