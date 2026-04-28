069dff2ec804912f09ac142f5e8c67f3d491196e
Mason McKeever <cheeseyfries2023@gmail.com>
2026-04-27 22:20:26 -0700

    wrangler fix 2

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello World' })
 })
 
-export default {
+
+// Crap i can't understand.
+const handler = {
   async fetch(request, env, ctx) {
-    return app.fetch(request, env, ctx)
+    return new Promise((resolve, reject) => {
+      const url = new URL(request.url)
+      const req2 = Object.assign(request, {
+        path: url.pathname,
+        query: Object.fromEntries(url.searchParams)
+      })
+      app(req2, {
+        setHeader: () => {},
+        end: (body) => resolve(new Response(body))
+      }, reject)
+    })
   }
 }
+
+export default handler
