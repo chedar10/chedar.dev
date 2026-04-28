82f8fd25c18cfeeab0cfedc3b9aff94ec65f3f89
Mason McKeever <cheeseyfries2023@gmail.com>
2026-04-27 22:24:36 -0700

    random commit

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-import express from 'express'
+import { Hono } from 'hono'
 
-const app = express()
+const app = new Hono()
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello World' })
+app.get('/api/hello', (c) => {
+  return c.json({ message: 'Hello World' })
 })
 
-
-// Crap i can't understand.
-const handler = {
-  async fetch(request, env, ctx) {
-    return new Promise((resolve, reject) => {
-      const url = new URL(request.url)
-      const req2 = Object.assign(request, {
-        path: url.pathname,
-        query: Object.fromEntries(url.searchParams)
-      })
-      app(req2, {
-        setHeader: () => {},
-        end: (body) => resolve(new Response(body))
-      }, reject)
-    })
-  }
-}
-
-export default handler
+export default app
