df8bcfbc59cfbc0381124d5036b9a6a6688732d3
Mason McKeever <cheeseyfries2023@gmail.com>
2026-04-28 07:03:35 -0700

    random commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import { Hono } from 'hono'
+import path from 'path'
 
 const app = new Hono()
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-
+// 1. API Routes
 app.get('/api/hewo', (c) => {
   return c.json({ message: 'Hello World' })
 })
 
 app.get('/favicon.ico', (c) => {
   return c.json({ message: 'No favicon' })
+})
+
+app.notFound((c) => {
+  return c.sendFile(path.join('./public', '404.html'),'text/html')
 })
 
 export default app
