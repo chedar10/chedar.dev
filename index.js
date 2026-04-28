ff06e2df82c60f4b0b8786624f8736e46431efcb
Mason McKeever <cheeseyfries2023@gmail.com>
2026-04-28 07:21:16 -0700

    random commit

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-// Epic asset serving
 app.notFound(async (c) => {
-  const url = new URL(c.req.url)
-  url.pathname = '/404.html'
-  const response = await c.env.ASSETS.fetch(url.toString())
-  return new Response(response.body, {
-    ...response,
-    status: 404
-  })
+    const url = new URL(c.req.url)
+    url.pathname = '/404.html'
+    const response = await c.env.ASSETS.fetch(url.toString())
+    return new Response(response.body, {
+        ...response,
+        status: 404
+    })
 })
 
 export default app
